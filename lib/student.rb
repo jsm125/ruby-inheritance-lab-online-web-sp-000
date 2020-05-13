f4dd0bf08f58a9ab0bdc1a947fa809b350c73761
Matt Im <matthew.seiim@gmail.com>
2020-05-13 20:22:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,4 +5,13 @@ class Student < User
   def initialize(knowledge=[])
     @knowledge = knowledge
   end
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    @knowledge
+  end
+  
 end
